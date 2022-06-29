58395e73deaff4bf092885eacbb32d4b9dd945c9
joaomarti755 <joaomarti755@gmail.com>
2022-06-29 18:59:32 -0300

    Correção de erros

---
+++ LocadoraForm/jao/LocadoraFisicaServico.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraForm.jao
+{
+    internal class LocadoraFisicaServico
+    {
+        private List<Endereco> enderecos;
+
+        public LocadoraFisicaServico()
+        {
+            enderecos = new List<Endereco>();
+        }
+
+        public void Adicionar(Endereco endereco)
+        {
+            enderecos.Add(endereco);
+
+            SalvarArquivo();
+        }
+
+        private void SalvarArquivo()
+        {
+            var enderecoEmJson = JsonConvert.SerializeObject(enderecos);
+            File.WriteAllText("enderecoLocadoraFisica.json", enderecoEmJson);
+        }
+    }
+}
