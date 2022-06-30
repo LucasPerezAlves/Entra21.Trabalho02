00b91539d01944279bb53d01d16ee7fc64302fe7
GustavoPrim <gugahprm@gmail.com>
2022-06-30 20:09:11 -0300

    Continuação dos métodos do FilmesForm

--- LocadoraForm/Prim/Filme.cs
+++ LocadoraForm/Prim/Filme.cs
@@ -1,10 +1,12 @@
-﻿namespace LocadoraForm.Prim
+﻿using System;
+
+namespace LocadoraForm.Prim
 {
     internal class Filme
     {
         public int Codigo;
         public string NomeFilme;
         public GeneroFilme GeneroFilme;
-        public ClassificacaoIndicativa ClassificacaoIndicativa;
+        public DateTime AnoLancamento;
     }
 }
--- LocadoraForm/Prim/FilmesForm.cs
+++ LocadoraForm/Prim/FilmesForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using LocadoraForm.Perez;
 using LocadoraForm.Prim;
 
 // LINHAS COMENTADAS, FAZER CORREÇÃO
@@ -8,38 +9,40 @@ namespace LocadoraForm
     public partial class FilmesForm : Form
     {
         private FilmeServico filmeServico;
+        private ClienteServico clienteServico;
+
         public FilmesForm()
         {
             InitializeComponent();
 
             filmeServico = new FilmeServico();
+
+            clienteServico = new ClienteServico();
+
+            PreencherDataGrideViewComFilmes();
+            PreencherComboBoxGeneroFilme();
         }
 
         private void LimparCampos()
         {
             textBoxNomeFilme.Clear();
             comboBoxGeneroFilme.ResetText();
-            radioButtonLivre.Checked = false;
-            radioButton12Anos.Checked = false;
-            radioButton16Anos.Checked = false;
-            radioButton18Anos.Checked = false;
+            maskedTextBox1.Clear();
         }
 
         private void FilmesForm_Load(object sender, EventArgs e)
         {
-
+            PreencherDataGrideViewComFilmes();
         }
 
         private void buttonEditar_Click(object sender, EventArgs e)
         {
-
+            DadosParaEditar();
         }
 
         private void buttonApagar_Click(object sender, EventArgs e)
         {
-            var linhasSelecionadas = dataGridView1.SelectedRows.Count;
-
-            if (linhasSelecionadas == 0)
+            if (dataGridView1.SelectedRows.Count == 0)
             {
                 MessageBox.Show("Selecione um paciente para apagar");
 
@@ -48,12 +51,24 @@ namespace LocadoraForm
 
             var opcaoEscolhida = MessageBox.Show("Deseja realmente apagar?", "AVISO", MessageBoxButtons.YesNo);
 
-            if (opcaoEscolhida == DialogResult.Yes)
+            if (opcaoEscolhida != DialogResult.Yes)
             {
-                var linhaSelecionada = dataGridView1.SelectedRows[0];
+                MessageBox.Show("Filme não retirado do cadastro");
 
-                var codigoSelecionado = Convert.ToInt32(linhaSelecionada.Cells[0].Value);
+                return;
             }
+
+            var linhaSelecionada = dataGridView1.SelectedRows[0];
+
+            var codigo = Convert.ToInt32(linhaSelecionada.Cells[0].Value);
+
+            var filme = filmeServico.ObterPorCodigo(codigo);
+
+            filmeServico.Apagar(filme);
+
+            PreencherDataGrideViewComFilmes();
+
+            dataGridView1.ClearSelection();
         }
 
         private void comboBoxGeneroFilme_SelectedIndexChanged(object sender, EventArgs e)
@@ -70,7 +85,7 @@ namespace LocadoraForm
         {
             var nomeFilme = textBoxNomeFilme.Text;
             var generoFilme = comboBoxGeneroFilme.SelectedItem.ToString();
-            //var classificacaoIndicativa = radioButtonLivre.Checked;
+            var anoLancamento = maskedTextBox1.Text;
         }
 
         private void DadosParaEditar()
@@ -88,9 +103,66 @@ namespace LocadoraForm
 
             textBoxNomeFilme.Text = filme.NomeFilme;
             comboBoxGeneroFilme.Text = filme.GeneroFilme.ToString();
-            //radioButtonLivre
+            maskedTextBox1.Text = filme.AnoLancamento.ToString();
         }
 
+        private void PreencherDataGrideViewComFilmes()
+        {
+            var filmes = filmeServico.ObterTodos();
 
+            dataGridView1.Rows.Clear();
+
+            dataGridView1.ClearSelection();
+
+            for (var i = 0; i < filmes.Count; i++)
+            {
+                var filme = filmes[i];
+
+                dataGridView1.Rows.Add(new object[]
+                {
+                    filme.Codigo,
+                    filme.NomeFilme,
+                    filme.GeneroFilme,
+                    filme.AnoLancamento
+                });
+            }
+        }
+
+        private void PreencherComboBoxGeneroFilme()
+        {
+            var generos = clienteServico.ObterTodos();
+
+            for (var i = 0; i < generos.Count; i++)
+            {
+                var cliente = generos[i];
+                comboBoxGeneroFilme.Items.Add(generos); 
+            }
+        }
+
+        private void ValidarDados(string nomeFilme, string generoFilme, string anoLancamento)
+        {
+            try
+            {
+                if (nomeFilme.Length < 3 && nomeFilme.Length > 30)
+                {
+                    MessageBox.Show("Nome do filme é inválido");
+
+                    textBoxNomeFilme.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nome do filme é inválido");
+            }
+
+            if (comboBoxGeneroFilme.SelectedIndex == -1)
+            {
+                MessageBox.Show("Escolha um paciente");
+
+                comboBoxGeneroFilme.DroppedDown = true;
+            }
+
+
+        }
     }
 }
