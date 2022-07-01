9152db2743db745a663a4f56765e38ec09d34fd0
Lucas Perez <perezcfnpbnu@gmail.com>
2022-06-30 21:22:22 -0300

    realizado códigos no form assim como correção em algumas partes

--- LocadoraForm/Perez/ClientesForm.Designer.cs
+++ LocadoraForm/Perez/ClientesForm.Designer.cs
@@ -36,18 +36,23 @@
             this.labelCep = new System.Windows.Forms.Label();
             this.labelEndereco = new System.Windows.Forms.Label();
             this.textBoxEndereco = new System.Windows.Forms.TextBox();
-            this.radioButtonMasculino = new System.Windows.Forms.RadioButton();
-            this.labelSexo = new System.Windows.Forms.Label();
-            this.radioButtonFeminino = new System.Windows.Forms.RadioButton();
             this.buttonEditar = new System.Windows.Forms.Button();
             this.buttonApagar = new System.Windows.Forms.Button();
             this.buttonSalvar = new System.Windows.Forms.Button();
+            this.maskedTextBoxCep = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonMasculino = new System.Windows.Forms.RadioButton();
+            this.radioButtonFeminino = new System.Windows.Forms.RadioButton();
+            this.ColumnCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCep = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnEndereco = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnSexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnIdade = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.maskedTextBoxCep = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -56,39 +61,47 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnCodigo,
             this.ColumnNome,
+            this.ColumnCep,
             this.ColumnEndereco,
             this.ColumnSexo,
             this.ColumnIdade});
-            this.dataGridView1.Location = new System.Drawing.Point(250, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(333, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(549, 425);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(845, 523);
             this.dataGridView1.TabIndex = 0;
             // 
             // labelNome
             // 
             this.labelNome.AutoSize = true;
-            this.labelNome.Location = new System.Drawing.Point(12, 29);
+            this.labelNome.Location = new System.Drawing.Point(16, 36);
+            this.labelNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNome.Name = "labelNome";
-            this.labelNome.Size = new System.Drawing.Size(35, 13);
+            this.labelNome.Size = new System.Drawing.Size(44, 16);
             this.labelNome.TabIndex = 1;
             this.labelNome.Text = "Nome";
             // 
             // textBoxNome
             // 
-            this.textBoxNome.Location = new System.Drawing.Point(12, 45);
+            this.textBoxNome.Location = new System.Drawing.Point(16, 55);
+            this.textBoxNome.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxNome.Name = "textBoxNome";
-            this.textBoxNome.Size = new System.Drawing.Size(232, 20);
+            this.textBoxNome.Size = new System.Drawing.Size(308, 22);
             this.textBoxNome.TabIndex = 2;
             this.textBoxNome.TextChanged += new System.EventHandler(this.textBoxNome_TextChanged);
             // 
             // radioButtonMaiorIdade
             // 
             this.radioButtonMaiorIdade.AutoSize = true;
-            this.radioButtonMaiorIdade.Location = new System.Drawing.Point(149, 74);
+            this.radioButtonMaiorIdade.Location = new System.Drawing.Point(7, 59);
+            this.radioButtonMaiorIdade.Margin = new System.Windows.Forms.Padding(4);
             this.radioButtonMaiorIdade.Name = "radioButtonMaiorIdade";
-            this.radioButtonMaiorIdade.Size = new System.Drawing.Size(95, 17);
+            this.radioButtonMaiorIdade.Size = new System.Drawing.Size(119, 20);
             this.radioButtonMaiorIdade.TabIndex = 3;
             this.radioButtonMaiorIdade.Text = "Maior de idade";
             this.radioButtonMaiorIdade.UseVisualStyleBackColor = true;
@@ -97,73 +110,51 @@
             // 
             this.radioButtonMenorIdade.AutoSize = true;
             this.radioButtonMenorIdade.Checked = true;
-            this.radioButtonMenorIdade.Location = new System.Drawing.Point(149, 97);
+            this.radioButtonMenorIdade.Location = new System.Drawing.Point(7, 31);
+            this.radioButtonMenorIdade.Margin = new System.Windows.Forms.Padding(4);
             this.radioButtonMenorIdade.Name = "radioButtonMenorIdade";
-            this.radioButtonMenorIdade.Size = new System.Drawing.Size(99, 17);
+            this.radioButtonMenorIdade.Size = new System.Drawing.Size(123, 20);
             this.radioButtonMenorIdade.TabIndex = 4;
             this.radioButtonMenorIdade.TabStop = true;
             this.radioButtonMenorIdade.Text = "Menor de idade";
             this.radioButtonMenorIdade.UseVisualStyleBackColor = true;
+            this.radioButtonMenorIdade.CheckedChanged += new System.EventHandler(this.radioButtonMenorIdade_CheckedChanged);
             // 
             // labelCep
             // 
             this.labelCep.AutoSize = true;
-            this.labelCep.Location = new System.Drawing.Point(9, 78);
+            this.labelCep.Location = new System.Drawing.Point(12, 260);
+            this.labelCep.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCep.Name = "labelCep";
-            this.labelCep.Size = new System.Drawing.Size(28, 13);
+            this.labelCep.Size = new System.Drawing.Size(34, 16);
             this.labelCep.TabIndex = 5;
             this.labelCep.Text = "CEP";
             // 
             // labelEndereco
             // 
             this.labelEndereco.AutoSize = true;
-            this.labelEndereco.Location = new System.Drawing.Point(9, 127);
+            this.labelEndereco.Location = new System.Drawing.Point(13, 323);
+            this.labelEndereco.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelEndereco.Name = "labelEndereco";
-            this.labelEndereco.Size = new System.Drawing.Size(95, 13);
+            this.labelEndereco.Size = new System.Drawing.Size(120, 16);
             this.labelEndereco.TabIndex = 7;
             this.labelEndereco.Text = "Endereço Digitado";
+            this.labelEndereco.Leave += new System.EventHandler(this.maskedTextBoxCep_MouseLeave);
             // 
             // textBoxEndereco
             // 
-            this.textBoxEndereco.Location = new System.Drawing.Point(12, 143);
+            this.textBoxEndereco.Location = new System.Drawing.Point(13, 343);
+            this.textBoxEndereco.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxEndereco.Name = "textBoxEndereco";
-            this.textBoxEndereco.Size = new System.Drawing.Size(232, 20);
+            this.textBoxEndereco.Size = new System.Drawing.Size(308, 22);
             this.textBoxEndereco.TabIndex = 8;
-            // 
-            // radioButtonMasculino
-            // 
-            this.radioButtonMasculino.AutoSize = true;
-            this.radioButtonMasculino.Location = new System.Drawing.Point(82, 176);
-            this.radioButtonMasculino.Name = "radioButtonMasculino";
-            this.radioButtonMasculino.Size = new System.Drawing.Size(73, 17);
-            this.radioButtonMasculino.TabIndex = 9;
-            this.radioButtonMasculino.Text = "Masculino";
-            this.radioButtonMasculino.UseVisualStyleBackColor = true;
-            // 
-            // labelSexo
-            // 
-            this.labelSexo.AutoSize = true;
-            this.labelSexo.Location = new System.Drawing.Point(12, 178);
-            this.labelSexo.Name = "labelSexo";
-            this.labelSexo.Size = new System.Drawing.Size(34, 13);
-            this.labelSexo.TabIndex = 10;
-            this.labelSexo.Text = "Sexo:";
-            // 
-            // radioButtonFeminino
-            // 
-            this.radioButtonFeminino.AutoSize = true;
-            this.radioButtonFeminino.Location = new System.Drawing.Point(177, 176);
-            this.radioButtonFeminino.Name = "radioButtonFeminino";
-            this.radioButtonFeminino.Size = new System.Drawing.Size(67, 17);
-            this.radioButtonFeminino.TabIndex = 11;
-            this.radioButtonFeminino.Text = "Feminino";
-            this.radioButtonFeminino.UseVisualStyleBackColor = true;
             // 
             // buttonEditar
             // 
-            this.buttonEditar.Location = new System.Drawing.Point(53, 12);
+            this.buttonEditar.Location = new System.Drawing.Point(71, 15);
+            this.buttonEditar.Margin = new System.Windows.Forms.Padding(4);
             this.buttonEditar.Name = "buttonEditar";
-            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.Size = new System.Drawing.Size(100, 28);
             this.buttonEditar.TabIndex = 12;
             this.buttonEditar.Text = "Editar";
             this.buttonEditar.UseVisualStyleBackColor = true;
@@ -171,9 +162,10 @@
             // 
             // buttonApagar
             // 
-            this.buttonApagar.Location = new System.Drawing.Point(134, 12);
+            this.buttonApagar.Location = new System.Drawing.Point(179, 15);
+            this.buttonApagar.Margin = new System.Windows.Forms.Padding(4);
             this.buttonApagar.Name = "buttonApagar";
-            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.Size = new System.Drawing.Size(100, 28);
             this.buttonApagar.TabIndex = 13;
             this.buttonApagar.Text = "Apagar";
             this.buttonApagar.UseVisualStyleBackColor = true;
@@ -181,70 +173,143 @@
             // 
             // buttonSalvar
             // 
-            this.buttonSalvar.Location = new System.Drawing.Point(169, 199);
+            this.buttonSalvar.Location = new System.Drawing.Point(221, 392);
+            this.buttonSalvar.Margin = new System.Windows.Forms.Padding(4);
             this.buttonSalvar.Name = "buttonSalvar";
-            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.Size = new System.Drawing.Size(100, 28);
             this.buttonSalvar.TabIndex = 14;
             this.buttonSalvar.Text = "Salvar";
             this.buttonSalvar.UseVisualStyleBackColor = true;
             this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
             // 
+            // maskedTextBoxCep
+            // 
+            this.maskedTextBoxCep.Location = new System.Drawing.Point(13, 280);
+            this.maskedTextBoxCep.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBoxCep.Mask = "99999-999";
+            this.maskedTextBoxCep.Name = "maskedTextBoxCep";
+            this.maskedTextBoxCep.Size = new System.Drawing.Size(173, 22);
+            this.maskedTextBoxCep.TabIndex = 15;
+            this.maskedTextBoxCep.Leave += new System.EventHandler(this.maskedTextBoxCep_Leave);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonMenorIdade);
+            this.groupBox1.Controls.Add(this.radioButtonMaiorIdade);
+            this.groupBox1.Location = new System.Drawing.Point(15, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(149, 113);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "idade:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonFeminino);
+            this.groupBox2.Controls.Add(this.radioButtonMasculino);
+            this.groupBox2.Location = new System.Drawing.Point(170, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(154, 113);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Genero:";
+            // 
+            // radioButtonMasculino
+            // 
+            this.radioButtonMasculino.AutoSize = true;
+            this.radioButtonMasculino.Location = new System.Drawing.Point(7, 22);
+            this.radioButtonMasculino.Name = "radioButtonMasculino";
+            this.radioButtonMasculino.Size = new System.Drawing.Size(89, 20);
+            this.radioButtonMasculino.TabIndex = 0;
+            this.radioButtonMasculino.TabStop = true;
+            this.radioButtonMasculino.Text = "Masculino";
+            this.radioButtonMasculino.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFeminino
+            // 
+            this.radioButtonFeminino.AutoSize = true;
+            this.radioButtonFeminino.Location = new System.Drawing.Point(7, 49);
+            this.radioButtonFeminino.Name = "radioButtonFeminino";
+            this.radioButtonFeminino.Size = new System.Drawing.Size(83, 20);
+            this.radioButtonFeminino.TabIndex = 1;
+            this.radioButtonFeminino.TabStop = true;
+            this.radioButtonFeminino.Text = "Feminino";
+            this.radioButtonFeminino.UseVisualStyleBackColor = true;
+            // 
+            // ColumnCodigo
+            // 
+            this.ColumnCodigo.HeaderText = "Codigo";
+            this.ColumnCodigo.MinimumWidth = 6;
+            this.ColumnCodigo.Name = "ColumnCodigo";
+            this.ColumnCodigo.ReadOnly = true;
+            this.ColumnCodigo.Width = 125;
+            // 
             // ColumnNome
             // 
             this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.MinimumWidth = 6;
             this.ColumnNome.Name = "ColumnNome";
             this.ColumnNome.ReadOnly = true;
+            this.ColumnNome.Width = 125;
+            // 
+            // ColumnCep
+            // 
+            this.ColumnCep.HeaderText = "CEP";
+            this.ColumnCep.MinimumWidth = 6;
+            this.ColumnCep.Name = "ColumnCep";
+            this.ColumnCep.ReadOnly = true;
+            this.ColumnCep.Width = 125;
             // 
             // ColumnEndereco
             // 
             this.ColumnEndereco.HeaderText = "Endereço";
+            this.ColumnEndereco.MinimumWidth = 6;
             this.ColumnEndereco.Name = "ColumnEndereco";
             this.ColumnEndereco.ReadOnly = true;
+            this.ColumnEndereco.Width = 125;
             // 
             // ColumnSexo
             // 
             this.ColumnSexo.HeaderText = "Sexo";
+            this.ColumnSexo.MinimumWidth = 6;
             this.ColumnSexo.Name = "ColumnSexo";
             this.ColumnSexo.ReadOnly = true;
+            this.ColumnSexo.Width = 125;
             // 
             // ColumnIdade
             // 
             this.ColumnIdade.HeaderText = "Maior/Menor de idade";
+            this.ColumnIdade.MinimumWidth = 6;
             this.ColumnIdade.Name = "ColumnIdade";
             this.ColumnIdade.ReadOnly = true;
-            // 
-            // maskedTextBoxCep
-            // 
-            this.maskedTextBoxCep.Location = new System.Drawing.Point(12, 97);
-            this.maskedTextBoxCep.Mask = "99999 -999";
-            this.maskedTextBoxCep.Name = "maskedTextBoxCep";
-            this.maskedTextBoxCep.Size = new System.Drawing.Size(131, 20);
-            this.maskedTextBoxCep.TabIndex = 15;
+            this.ColumnIdade.Width = 125;
             // 
             // ClientesForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1191, 554);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.maskedTextBoxCep);
             this.Controls.Add(this.buttonSalvar);
             this.Controls.Add(this.buttonApagar);
             this.Controls.Add(this.buttonEditar);
-            this.Controls.Add(this.radioButtonFeminino);
-            this.Controls.Add(this.labelSexo);
-            this.Controls.Add(this.radioButtonMasculino);
             this.Controls.Add(this.textBoxEndereco);
             this.Controls.Add(this.labelEndereco);
             this.Controls.Add(this.labelCep);
-            this.Controls.Add(this.radioButtonMenorIdade);
-            this.Controls.Add(this.radioButtonMaiorIdade);
             this.Controls.Add(this.textBoxNome);
             this.Controls.Add(this.labelNome);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ClientesForm";
             this.Text = "ClientesForm";
             this.Load += new System.EventHandler(this.ClientesForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -260,16 +325,19 @@
         private System.Windows.Forms.Label labelCep;
         private System.Windows.Forms.Label labelEndereco;
         private System.Windows.Forms.TextBox textBoxEndereco;
-        private System.Windows.Forms.RadioButton radioButtonMasculino;
-        private System.Windows.Forms.Label labelSexo;
-        private System.Windows.Forms.RadioButton radioButtonFeminino;
         private System.Windows.Forms.Button buttonEditar;
         private System.Windows.Forms.Button buttonApagar;
         private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxCep;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButtonFeminino;
+        private System.Windows.Forms.RadioButton radioButtonMasculino;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCodigo;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCep;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEndereco;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnSexo;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIdade;
-        private System.Windows.Forms.MaskedTextBox maskedTextBoxCep;
     }
 }
